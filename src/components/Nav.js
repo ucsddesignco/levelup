a4469f8ce28322954fc79f852c3faa0145e7fa34
paridhikhaitan <paridhi4699@gmail.com>
2020-09-02 20:19:41 +0530

    nav fixes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,41 +1,54 @@
-import React from 'react';
+import React from "react";
 
 class Nav extends React.Component {
-
   toggleHamburger = () => {
     if (this.props.mobile) {
       // toggle hamburger menu
       document.getElementById("hamburger").classList.toggle("is-active");
-      document.getElementById("nav").classList.toggle("hideNav"); 
+      document.getElementById("nav").classList.toggle("hideNav");
 
       // toggle nav z-index
       let nav = document.getElementById("nav");
-      nav.style.zIndex = window.getComputedStyle(nav).getPropertyValue("z-index") === "0" ? "3" : "0";
+      nav.style.zIndex =
+        window.getComputedStyle(nav).getPropertyValue("z-index") === "0"
+          ? "3"
+          : "0";
 
       // toggle prevent scroll
-      document.body.style.overflow = window.getComputedStyle(document.body).getPropertyValue("overflow") === "visible" ? "hidden" : "visible";
+      document.body.style.overflow =
+        window.getComputedStyle(document.body).getPropertyValue("overflow") ===
+        "visible"
+          ? "hidden"
+          : "visible";
 
       let gradient = document.getElementsByClassName("gradient");
       for (let i = 0; i < gradient.length; i++) {
-        gradient[i].style.opacity = window.getComputedStyle(gradient[i]).getPropertyValue("opacity") === "0" ? "100" : "0";
+        gradient[i].style.opacity =
+          window.getComputedStyle(gradient[i]).getPropertyValue("opacity") ===
+          "0"
+            ? "100"
+            : "0";
       }
     }
-  }
+  };
 
   render() {
     return (
-      <div 
-        id="nav" 
-        className="nav hideNav" 
-        style={{ transform: this.props.mobile ? 'translate(0)' : `${this.props.navPosition}`}}
+      <div
+        id="nav"
+        className="nav hideNav"
+        style={{
+          transform: this.props.mobile
+            ? "translate(0)"
+            : `${this.props.navPosition}`
+        }}
       >
-        
-        <div 
+        <div
           id="hamburger"
-          className="hamburger hamburger--spin" 
+          className="hamburger hamburger--spin"
           type="button"
           onClick={this.toggleHamburger}
-          style={{display: this.props.mobile ? "block" : "none" }}
+          style={{ display: this.props.mobile ? "block" : "none" }}
         >
           <div className="hamburger-box">
             <div className="hamburger-inner"></div>
@@ -44,24 +57,50 @@ class Nav extends React.Component {
 
         <div className="navHeader">
           <h3>Website Content</h3>
-          <img 
+          <img
             id="minimizeBtn"
-            onClick={() => this.props.minimizeNav()} 
-            src={require(this.props.minimized ? "../images/maximize.png" : "../images/minimize.png")} 
-            alt="maximize" 
+            onClick={() => this.props.minimizeNav()}
+            src={require(this.props.minimized
+              ? "../images/maximize.png"
+              : "../images/minimize.png")}
+            alt="maximize"
           />
-        </div>
-        
-        <div className="gradient gradientTransition">
-          <span>About</span>
-          <a 
-            href="#about" 
-            onClick={() => this.props.setActiveIndex(0)}
-            className={ this.props.activeIndex === 0 ? "active" : "" }
-          >&nbsp;</a>
         </div>
 
         <div className="gradient gradientTransition">
+          <span>About</span>
+          <a
+            href="#about"
+            onClick={() => this.props.setActiveIndex(0)}
+            className={this.props.activeIndex === 0 ? "active" : ""}
+          >
+            &nbsp;
+          </a>
+        </div>
+
+        <div className="gradient gradientTransition">
+          <span>Showcase</span>
+          <a
+            href="#showcase"
+            onClick={() => this.props.setActiveIndex(1)}
+            className={this.props.activeIndex === 1 ? "active" : ""}
+          >
+            &nbsp;
+          </a>
+        </div>
+
+        <div className="gradient gradientTransition">
+          <span>Meet The Level Uppers</span>
+          <a
+            href="#leveluppers"
+            onClick={() => this.props.setActiveIndex(2)}
+            className={this.props.activeIndex === 2 ? "active" : ""}
+          >
+            &nbsp;
+          </a>
+        </div>
+
+        {/* <div className="gradient gradientTransition">
           <span>Who is it for?</span>
           <a 
             href="#who" 
@@ -95,10 +134,9 @@ class Nav extends React.Component {
             onClick={() => this.props.setActiveIndex(4)}
             className={ this.props.activeIndex === 4 ? "active" : "" }
           >&nbsp;</a>
-        </div>
-
+        </div> */}
       </div>
-    )
+    );
   }
 }
 
